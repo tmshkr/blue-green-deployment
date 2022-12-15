f03d7b32d36801d7fa669c9f799c497e7e2be789
Tim Shaker <tim@tmshkr.com>
2022-12-14 20:10:58 -0800

    swap-environment-cnames

--- .platform/hooks/predeploy/init.sh
+++ .platform/hooks/predeploy/init.sh
@@ -4,3 +4,11 @@ set -e
 EB_ENVIRONMENT=$(/opt/elasticbeanstalk/bin/get-config container -k environment_name)
 printf "\nEB_ENVIRONMENT=$EB_ENVIRONMENT" >> .env
 printf "\n" >> .env
+
+export $(cat .env | xargs)
+
+if [ "$BRANCH" = "main" ]; then
+  aws elasticbeanstalk swap-environment-cnames --source-environment-name $OLD_ENV --destination-environment-name $EB_ENVIRONMENT
+else
+  echo "Not swapping CNAMEs for branch $BRANCH"
+fi
