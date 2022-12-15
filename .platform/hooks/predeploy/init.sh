662a4a438520252f6778ee56b531a8d2ab415ed3
Tim Shaker <tim@tmshkr.com>
2022-12-14 20:20:27 -0800

    add AWS_REGION

--- .platform/hooks/predeploy/init.sh
+++ .platform/hooks/predeploy/init.sh
@@ -8,7 +8,7 @@ printf "\n" >> .env
 export $(cat .env | xargs)
 
 if [ "$BRANCH" = "main" ]; then
-  aws elasticbeanstalk swap-environment-cnames --source-environment-name $OLD_ENV --destination-environment-name $EB_ENVIRONMENT
+  aws elasticbeanstalk swap-environment-cnames --source-environment-name $OLD_ENV --destination-environment-name $EB_ENVIRONMENT --region $AWS_REGION
 else
   echo "Not swapping CNAMEs for branch $BRANCH"
 fi
