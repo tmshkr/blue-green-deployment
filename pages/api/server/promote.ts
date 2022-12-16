c321bdc7143230d6df15d15fd7f17bc6e0184196
Tim Shaker <tim@tmshkr.com>
2022-12-15 21:49:08 -0800

    add promote endpoint

---
+++ pages/api/server/promote.ts
@@ -0,0 +1,16 @@
+import { exec } from "child_process";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.headers.authorization !== "Bearer " + process.env.API_KEY) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  if (req.method !== "POST") {
+    res.status(405).send("Method Not Allowed");
+    return;
+  }
+  res.status(202).send("Accepted");
+
+  exec("./promote.sh");
+}
