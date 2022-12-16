7b2ae94e1880515bd5d5e1857095ec590b0db21e
Tim Shaker <tim@tmshkr.com>
2022-12-16 13:18:35 -0800

    add promote job

--- .platform/hooks/predeploy/get-env-vars.sh
+++ .platform/hooks/predeploy/get-env-vars.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+# get environment
+ENVIRONMENT=$(cat ENVIRONMENT)
+
 # get environment variables
 EB_ENVIRONMENT=$(/opt/elasticbeanstalk/bin/get-config container -k environment_name)
 CONFIG_S3_BUCKET=$(/opt/elasticbeanstalk/bin/get-config environment -k CONFIG_S3_BUCKET)
-aws s3 cp s3://$CONFIG_S3_BUCKET/staging.env .env
+aws s3 cp s3://$CONFIG_S3_BUCKET/$ENVIRONMENT.env .env
 
 printf "\n" >> .env
 printf "EB_ENVIRONMENT=$EB_ENVIRONMENT" >> .env
