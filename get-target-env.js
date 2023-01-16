a9df954e115eeec9eed2a0e445151bc7b9046dea
Tim Shaker <tim@tmshkr.com>
2023-01-15 23:33:52 -0800

    use template

--- get-target-env.js
+++ get-target-env.js
@@ -43,7 +43,7 @@ function getTargetEnv() {
 function createEnvironment(envName) {
   console.log("Creating new environment... ");
   const stdout = execSync(
-    `aws elasticbeanstalk create-environment --application-name ${APP_NAME} --environment-name ${envName} --cname-prefix ${STAGING_CNAME} --solution-stack-name "64bit Amazon Linux 2 v3.5.3 running Docker" --option-settings Namespace=aws:autoscaling:launchconfiguration,OptionName=IamInstanceProfile,Value=aws-elasticbeanstalk-ec2-role`
+    `aws elasticbeanstalk create-environment --application-name ${APP_NAME} --environment-name ${envName} --cname-prefix ${STAGING_CNAME} --template-name my-template`
   );
 
   const newEnv = JSON.parse(stdout);
